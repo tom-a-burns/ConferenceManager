0647585d622df908378c1e39a71b364ed87cfa93
Tom Burns <tom.burns@leephilipgroup.com>
2020-01-03 16:46:58 -0500

    test templates

--- src/app/conferences/services/conference-data.service.spec.ts
+++ src/app/conferences/services/conference-data.service.spec.ts
@@ -9,4 +9,20 @@ describe('ConferenceDataService', () => {
     const service: ConferenceDataService = TestBed.get(ConferenceDataService);
     expect(service).toBeTruthy();
   });
+
+  it('addConference()',() => {
+    expect(true);
+  });
+
+  it('updateConference()',() => {
+    expect(true);
+  });
+
+  it('getConferenceList()',() => {
+    expect(true);
+  });
+
+  it('getConference()',() => {
+    expect(true);
+  });
 });
--- src/app/conferences/services/conference-forms.service.spec.ts
+++ src/app/conferences/services/conference-forms.service.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ConferenceFormsService } from './conference-forms.service';
 
@@ -8,5 +9,25 @@ describe('ConferenceFormsService', () => {
   it('should be created', () => {
     const service: ConferenceFormsService = TestBed.get(ConferenceFormsService);
     expect(service).toBeTruthy();
+  });
+
+  it('addDivisionFormGroup',() => {
+    expect(true);
+  });
+
+  it('removeDivisionFormGroup',() => {
+    expect(true);
+  });
+
+  it('normalize',() => {
+    expect(true);
+  });
+
+  it('getConferenceForm',() => {
+    expect(true);
+  });
+
+  it('setConferenceFormValues',() => {
+    expect(true);
   });
 });
